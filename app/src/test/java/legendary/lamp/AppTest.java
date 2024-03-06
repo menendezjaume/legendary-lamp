e89e8abec308e46b16c2e6b72efe07347d96e9a0
Miguel Angel <m.menendezjaume@gmail.com>
2024-03-06 15:12:50 +0100

    Test de suma creado

--- app/src/test/java/legendary/lamp/AppTest.java
+++ app/src/test/java/legendary/lamp/AppTest.java
@@ -7,8 +7,15 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
-    @Test void appHasAGreeting() {
+    @Test
+    void appHasAGreeting() {
         App classUnderTest = new App();
         assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+    }
+
+    @Test
+    void appSumaCorrecta() {
+        App classUnderTest = new App();
+        assertEquals(6, classUnderTest.sumar(3, 2));
     }
 }
